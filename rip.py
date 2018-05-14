e6f224acf6f8ac403c3b9ea1b6bf61f144a42eb7
jlung20 <jefflung248@yahoo.com>
2018-05-14 04:19:34 -0700

    Update rip.py

--- rip.py
+++ rip.py
@@ -5,34 +5,34 @@ Created on Mon May 14 03:03:25 2018
 @author: jlung20
 """
 
-import json
 import requests
 import time
 import string
 import random
 
 url = 'http://upe.42069.fun/WcDGr'
+
+# continues guessing ad infinitum
 while(True):
+    # sleep to not overload the service
     time.sleep(1)
     r = requests.get(url)
+    # print to get status of game
     try:
         print(r.json())
+    # in the case that there's a JSONDecodeError
     except ValueError:
         pass
-    #pyth = json.loads(r.text)
-    #print(pyth['state'])
+    
+    # sleep to not overload the service
     time.sleep(1)
-    lol = random.choice(string.ascii_lowercase)
-    char = { 'guess' : lol }
-    resp = requests.post(url, json=char)
+    rand_char = random.choice(string.ascii_lowercase)
+    guess = { 'guess' : rand_char }
+    resp = requests.post(url, json=guess)
+    # print to get status of game
     try:
         print(resp.json())
+    # in the case that there's a JSONDecodeError
     except ValueError:
         pass 
-'''while(True):
-time.sleep(1)
-resp = requests.post(url, json=char)
-print(resp.json()) '''
-#time.sleep(1)
-#r = requests.get(url)
-#print(r.json())
+    
